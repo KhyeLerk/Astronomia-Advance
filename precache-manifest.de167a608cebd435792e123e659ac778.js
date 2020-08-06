1e05450c421e54b9d474fe3ffed827f2bf42076b
KhyeLerk <Khye.19@ichat.sp.edu.sg>
2020-08-06 16:20:45 +0800

    Updates

---
+++ precache-manifest.de167a608cebd435792e123e659ac778.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f72dc8b514fd3d357df6e9c391a777",
+    "url": "/Astronomia-Advance/index.html"
+  },
+  {
+    "revision": "179f64615709db23ce86",
+    "url": "/Astronomia-Advance/static/css/4.341c5ad7.chunk.css"
+  },
+  {
+    "revision": "20d616a7d85d8c2e1f0c",
+    "url": "/Astronomia-Advance/static/css/main.4aedc49e.chunk.css"
+  },
+  {
+    "revision": "e3cbfe303b6f3acd1407",
+    "url": "/Astronomia-Advance/static/js/0.b0fae201.chunk.js"
+  },
+  {
+    "revision": "4ba1d83796129be53a71",
+    "url": "/Astronomia-Advance/static/js/10.635f92d4.chunk.js"
+  },
+  {
+    "revision": "e169288549c9e2725769",
+    "url": "/Astronomia-Advance/static/js/11.434cb21f.chunk.js"
+  },
+  {
+    "revision": "874386020a838a39f066",
+    "url": "/Astronomia-Advance/static/js/12.e8e8a32b.chunk.js"
+  },
+  {
+    "revision": "47b76004a7223e3e8e53",
+    "url": "/Astronomia-Advance/static/js/13.1dff60d8.chunk.js"
+  },
+  {
+    "revision": "543bb61d10de68f530da",
+    "url": "/Astronomia-Advance/static/js/14.28ea6c1d.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/Astronomia-Advance/static/js/14.28ea6c1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dd06105f7825e14ddb4",
+    "url": "/Astronomia-Advance/static/js/15.a31d7379.chunk.js"
+  },
+  {
+    "revision": "7e850cb98523e6648d61",
+    "url": "/Astronomia-Advance/static/js/16.9d13f837.chunk.js"
+  },
+  {
+    "revision": "d820b0da58f7b2f58b08",
+    "url": "/Astronomia-Advance/static/js/17.fe1f12c3.chunk.js"
+  },
+  {
+    "revision": "58f43b93a70041c86607",
+    "url": "/Astronomia-Advance/static/js/18.04246491.chunk.js"
+  },
+  {
+    "revision": "3d5fa63313e6df0173d8",
+    "url": "/Astronomia-Advance/static/js/19.9973221f.chunk.js"
+  },
+  {
+    "revision": "eddabc9763c98ba566fa",
+    "url": "/Astronomia-Advance/static/js/20.825bdd37.chunk.js"
+  },
+  {
+    "revision": "97f2ed45b1a628e7bb65",
+    "url": "/Astronomia-Advance/static/js/21.edd6be4a.chunk.js"
+  },
+  {
+    "revision": "a7b45667c11f3fa113a6",
+    "url": "/Astronomia-Advance/static/js/22.6a65e780.chunk.js"
+  },
+  {
+    "revision": "9fe571ede6d195f4cd2c",
+    "url": "/Astronomia-Advance/static/js/23.f829d29b.chunk.js"
+  },
+  {
+    "revision": "9cc663370aa7b2186f76",
+    "url": "/Astronomia-Advance/static/js/24.97c5472c.chunk.js"
+  },
+  {
+    "revision": "356fc84cd94b79ae9d3e",
+    "url": "/Astronomia-Advance/static/js/25.ad4ab322.chunk.js"
+  },
+  {
+    "revision": "618274f30c89f895f32e",
+    "url": "/Astronomia-Advance/static/js/26.b1e3a41d.chunk.js"
+  },
+  {
+    "revision": "e728b20d053ab774df9c",
+    "url": "/Astronomia-Advance/static/js/27.1e9213a1.chunk.js"
+  },
+  {
+    "revision": "267a03fca288cba7beb3",
+    "url": "/Astronomia-Advance/static/js/28.ff697f20.chunk.js"
+  },
+  {
+    "revision": "094157ca64f51c80c1e5",
+    "url": "/Astronomia-Advance/static/js/29.f7336163.chunk.js"
+  },
+  {
+    "revision": "946100fe23a9170990cc",
+    "url": "/Astronomia-Advance/static/js/30.18308c00.chunk.js"
+  },
+  {
+    "revision": "4b6c711b863228e12c68",
+    "url": "/Astronomia-Advance/static/js/31.97d26431.chunk.js"
+  },
+  {
+    "revision": "b57307757a013bd8acb6",
+    "url": "/Astronomia-Advance/static/js/32.14f37b36.chunk.js"
+  },
+  {
+    "revision": "6b440df53bd7b4cafb93",
+    "url": "/Astronomia-Advance/static/js/33.a5049167.chunk.js"
+  },
+  {
+    "revision": "d7aa63d2ce533a910437",
+    "url": "/Astronomia-Advance/static/js/34.db35b6c7.chunk.js"
+  },
+  {
+    "revision": "26da0a782b0ca821b9bc",
+    "url": "/Astronomia-Advance/static/js/35.12d49445.chunk.js"
+  },
+  {
+    "revision": "7b1426f6f2e4203418c5",
+    "url": "/Astronomia-Advance/static/js/36.a5661a15.chunk.js"
+  },
+  {
+    "revision": "3c2f1ad5d898e57013cf",
+    "url": "/Astronomia-Advance/static/js/37.61f7be18.chunk.js"
+  },
+  {
+    "revision": "80f6206ec3d058fb517d",
+    "url": "/Astronomia-Advance/static/js/38.2a802c56.chunk.js"
+  },
+  {
+    "revision": "73cb700d9bbb1efaeded",
+    "url": "/Astronomia-Advance/static/js/39.380e43f3.chunk.js"
+  },
+  {
+    "revision": "179f64615709db23ce86",
+    "url": "/Astronomia-Advance/static/js/4.583ea6b2.chunk.js"
+  },
+  {
+    "revision": "d1ec4551217b950b37ec99a7f91dfd82",
+    "url": "/Astronomia-Advance/static/js/4.583ea6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a34a080486700b3e4a1",
+    "url": "/Astronomia-Advance/static/js/40.3914e785.chunk.js"
+  },
+  {
+    "revision": "c7b3898a59e3760eb63f",
+    "url": "/Astronomia-Advance/static/js/41.fc46fd31.chunk.js"
+  },
+  {
+    "revision": "a9981cfab8bd5c4363b8",
+    "url": "/Astronomia-Advance/static/js/42.0c14fa00.chunk.js"
+  },
+  {
+    "revision": "3a837ac916fea0426b22",
+    "url": "/Astronomia-Advance/static/js/43.883bfce9.chunk.js"
+  },
+  {
+    "revision": "6779d17d131e25bad471",
+    "url": "/Astronomia-Advance/static/js/44.0df74c00.chunk.js"
+  },
+  {
+    "revision": "b5d1e99e03c3e8d10b63",
+    "url": "/Astronomia-Advance/static/js/45.7a3cb886.chunk.js"
+  },
+  {
+    "revision": "e332f33ff661aa17bd05",
+    "url": "/Astronomia-Advance/static/js/46.a6545ca4.chunk.js"
+  },
+  {
+    "revision": "45ad4c70e4d1a06b168a",
+    "url": "/Astronomia-Advance/static/js/47.1341a59d.chunk.js"
+  },
+  {
+    "revision": "deeb99f9171239be9050",
+    "url": "/Astronomia-Advance/static/js/48.81fb3ecf.chunk.js"
+  },
+  {
+    "revision": "3e7f3b10818548729079",
+    "url": "/Astronomia-Advance/static/js/49.bfdcc458.chunk.js"
+  },
+  {
+    "revision": "248d6c7c323148146552",
+    "url": "/Astronomia-Advance/static/js/5.edc8ae47.chunk.js"
+  },
+  {
+    "revision": "27e531c44b6668c68326",
+    "url": "/Astronomia-Advance/static/js/50.9505eada.chunk.js"
+  },
+  {
+    "revision": "5795ec1b42896da0787a",
+    "url": "/Astronomia-Advance/static/js/51.cfa2a78d.chunk.js"
+  },
+  {
+    "revision": "bffe5f790cb50b66b539",
+    "url": "/Astronomia-Advance/static/js/52.278c32d3.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/Astronomia-Advance/static/js/52.278c32d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5201ba49d5d3aa04ea6b",
+    "url": "/Astronomia-Advance/static/js/53.10df079b.chunk.js"
+  },
+  {
+    "revision": "a6eb57d26864dd13877e",
+    "url": "/Astronomia-Advance/static/js/54.c48d1701.chunk.js"
+  },
+  {
+    "revision": "f698833d9cae6e630306",
+    "url": "/Astronomia-Advance/static/js/55.50620168.chunk.js"
+  },
+  {
+    "revision": "43cea2e1828593bc9e0c",
+    "url": "/Astronomia-Advance/static/js/56.4898de21.chunk.js"
+  },
+  {
+    "revision": "8a823c8c575a180a8e13",
+    "url": "/Astronomia-Advance/static/js/57.031bc0d2.chunk.js"
+  },
+  {
+    "revision": "445c8c66d418ded8e0fa",
+    "url": "/Astronomia-Advance/static/js/58.1c158c86.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/Astronomia-Advance/static/js/58.1c158c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e086f7c506ec755aff",
+    "url": "/Astronomia-Advance/static/js/6.9368fa4b.chunk.js"
+  },
+  {
+    "revision": "b2717ff11c396b8f7c19",
+    "url": "/Astronomia-Advance/static/js/7.c8964eca.chunk.js"
+  },
+  {
+    "revision": "331888efc9e70e4c655a",
+    "url": "/Astronomia-Advance/static/js/8.f2fd0af5.chunk.js"
+  },
+  {
+    "revision": "c37e4bb728c6e7ee6a48",
+    "url": "/Astronomia-Advance/static/js/9.42f44577.chunk.js"
+  },
+  {
+    "revision": "20d616a7d85d8c2e1f0c",
+    "url": "/Astronomia-Advance/static/js/main.0e74d750.chunk.js"
+  },
+  {
+    "revision": "615854170264ad926303",
+    "url": "/Astronomia-Advance/static/js/polyfills-css-shim.a32f93d6.chunk.js"
+  },
+  {
+    "revision": "4977fd5d45ca00eb4d48",
+    "url": "/Astronomia-Advance/static/js/runtime-main.0b68b69c.js"
+  }
+]);
